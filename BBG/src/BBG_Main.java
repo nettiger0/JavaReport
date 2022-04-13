d0e215712d3f1119736ba61f181f59b843e12626
소슬바람 <@DESKTOP-5CK1PFU>
2022-04-13 23:25:55 +0900

    test2

--- BBG/src/BBG_Main.java
+++ BBG/src/BBG_Main.java
@@ -4,6 +4,8 @@ public class BBG_Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello, Wolrd!");
+		System.out.print("Hello, Wolrd!");
+		
 	}
 
 }
